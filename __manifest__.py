fc128a62072f5923839905426f1a80ea2fc03580
hsp@einfo-tech.com <hsp@einfo-tech.com>
2020-07-08 15:48:09 +0800

    git

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2020  HSP
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': "forbid debug mode",
+
+    'summary': """forbid debug mode
+    """,
+   'license': 'LGPL-3',
+   'description': """
+    forbid debug mode
+   """,
+    'author': 'HSP',
+    'website': "https://www.garage-kit.com",
+    'images': ['static/description/logo.png'],
+    'category': 'Tools',
+    'version': '13.0.1.0.0',
+  
+    'depends': ['base', 'base_setup'],
+    'data': [
+
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+}
---
+++ controllers/controller.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import http
+from odoo.addons.web.controllers.main import Home, ensure_db, redirect_with_hash
+from odoo.http import request
+
+
+class HSPHome(Home, http.Controller):
+
+    @http.route('/web', type='http', auth="none")
+    def web_client(self, s_action=None, **kw):
+        ensure_db()
+        if kw.get('debug') == "1" or kw.get('debug') == "assets" or kw.get('debug') == "assets,tests":
+            return redirect_with_hash('/web?debug=0')
+
+        return super(HSPHome, self).web_client(s_action=s_action, **kw)
+
+
